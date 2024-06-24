cb975a0e50ea10906977c381c6d2f9f4a9100cff
kaixuan477 <kxuan477@gmail.com>
2024-06-24 00:45:12 +0000

    DUHI implementation based on Clover - incomplete accuracy management and calculation

--- clover/test.py
+++ clover/test.py
@@ -4,6 +4,7 @@ import os
 import time
 from tqdm import tqdm
 from clover import load_config as lc
+#clover/align module for global alignment not imported
 
 class MyProcess(Process):
     """Process Class for Clover using DUHI's concept"""
@@ -15,7 +16,7 @@ class MyProcess(Process):
         self.data = data
         self.q_output = q_output
         self.q_output_temp = []
-        self.Cluster_size_threshold = self.config_dict['Cluster_size_threshold']
+        self.Cluster_size_threshold = self.config_dict['Cluster_size_threshold'] #unused
         self.ref_list = {}
         self.ref_dict = {}
         self.ref_error_dict = {}
@@ -24,45 +25,78 @@ class MyProcess(Process):
         self.index_list = []
         self.now_clust_threshold = self.config_dict['now_clust_threshold']
         self.read_len = self.config_dict['read_len']
-        self.dna_tree_nums = self.config_dict['end_tree_len']
-        self.fuzz_list = [self.config_dict['thd_tree_loc'], self.config_dict['four_tree_loc'], self.config_dict['other_tree_len']]
+        #self.dna_tree_nums = self.config_dict['end_tree_len']
+        #self.fuzz_list = [self.config_dict['thd_tree_loc'], self.config_dict['four_tree_loc'], self.config_dict['other_tree_len']]
         self.loc_nums = self.config_dict['Vertical_drift']
         self.tag_nums = self.config_dict['tag_nums']
         self.align_swicth = self.config_dict['align_fuc']
-        self.fuzz_tree_nums = self.config_dict['Horizontal_drift']
-        self.h_index = self.config_dict['h_index_nums']
-        self.e_index = self.config_dict['e_index_nums']
+        #self.fuzz_tree_nums = self.config_dict['Horizontal_drift']
+        self.h_index = self.config_dict['h_index_nums'] #unused
+        self.e_index = self.config_dict['e_index_nums'] #unused
         self.test_num = 0
         self.file_format = "txt"
-        if 'input_path' in self.config_dict:
+
+        # Current implementation only uses .txt input format
+        if 'input_path' in self.config_dict: 
             if self.config_dict['input_path'][-1] == "a":
                 self.file_format = "fasta"
             elif self.config_dict['input_path'][-1] == "q":
                 self.file_format = "fastq"
 
-        # Initialize arrays for storing hash values and corresponding indices
-        self.front_hash_array = []
-        self.middle_hash_array = []
-        self.back_hash_array = []
+        # Threshold for inter-cluster distance based on DUHI hash formula - in config file
+        self.cluster_hash_threshold = self.config_dict['Cluster_hash_threshold']
+        # Initialize arrays for storing cluster hash values
+        self.front_hash_array = {}
+        self.middle_hash_array = {}
+        self.back_hash_array = {}
+        self.front_ref_list = {}
+        self.middle_ref_list = {}
+        self.back_ref_list = {}
     
     def hash_value(self, fragment):
-        """Calculate the hash value of an 8-character DNA fragment."""
+        """DUHI's hash formula for DNA fragment."""
         base_val = {'A': 1, 'T': 2, 'G': 3, 'C': 4}
         hash_val = sum((2 ** (7 - i)) * base_val[char] for i, char in enumerate(fragment))
         return hash_val
 
     def extract_fragments(self, dna_str):
-        """Extract the front, middle, and back fragments of the DNA strand."""
+        """Extract the middle-front, middle, and middle-back fragments of the DNA strand."""
         fragment_length = 8
-        middle_start = (len(dna_str) // 2) - (fragment_length // 2)
+        middle_start = self.read_len // 3
+        middle_end = 2 * middle_start
+        
         return (
-            dna_str[:fragment_length],
             dna_str[middle_start:middle_start + fragment_length],
-            dna_str[-fragment_length:]
+            dna_str[self.read_len // 2 - fragment_length // 2:self.read_len // 2 + fragment_length // 2],
+            dna_str[middle_end - fragment_length:middle_end]
         )
 
+    def update_hash(self, dna_num, section):
+        hv = 0
+        count = 0
+        if section == "front":
+            for hash_val, hash_count in self.front_ref_list[dna_num]:
+                if hash_count > count:
+                    hv = hash_val
+                    count = hash_count
+            self.front_hash_array[dna_num] = (hv, 0)
+        elif section == "middle":
+            for hash_val, hash_count in self.middle_ref_list[dna_num]:
+                if hash_count > count:
+                    hv = hash_val
+                    count = hash_count
+            self.middle_hash_array[dna_num] = (hv, 0)
+        elif section == "back":
+            for hash_val, hash_count in self.back_ref_list[dna_num]:
+                if hash_count > count:
+                    hv = hash_val
+                    count = hash_count
+            self.back_hash_array[dna_num] = (hv, 0)
+        else:
+            pass
+
     def cluster(self, read):
-        """Clover's clustering function using DUHI's concept."""
+        """DUHI's clustering algorithm implemented on Clover."""
         self.test_num += 1
         line_ = read.split()
         
@@ -74,7 +108,7 @@ class MyProcess(Process):
         else:
             dna_str = line_[1]
             dna_tag = line_[0]
-        
+        #h_index, e_index for dna primer not implemented, assumed 0
         if len(dna_str) < self.config_dict['read_len_min'] or "N" in dna_str:
             return
         
@@ -82,27 +116,78 @@ class MyProcess(Process):
         front_hash = self.hash_value(front)
         middle_hash = self.hash_value(middle)
         back_hash = self.hash_value(back)
-        
-        self.front_hash_array.append((front_hash, dna_num))
-        self.middle_hash_array.append((middle_hash, dna_num))
-        self.back_hash_array.append((back_hash, dna_num))
 
-        # Insert clustering logic based on hash arrays
+        # Clustering logic using hash arrays
+        # Create new fragments clusters for first dna
+        if self.front_hash_array == []:
+            self.front_hash_array[dna_num] = (front_hash, 1)
+            self.middle_hash_array[dna_num] = (middle_hash, 1)
+            self.back_hash_array[dna_num] = (back_hash, 1)
+            #self.ref_list[dna_num] = dna_str
+            self.front_ref_list[dna_num] = {front_hash: 1}
+            self.middle_ref_list[dna_num] = {middle_hash: 1}
+            self.back_ref_list[dna_num] = {back_hash: 1}
+            self.ref_dict[dna_num] = [dna_tag]
+            return
+        
         match_found = False
-        for array, hash_val in [(self.front_hash_array, front_hash), (self.middle_hash_array, middle_hash), (self.back_hash_array, back_hash)]:
-            for stored_hash, stored_index in array:
-                if stored_hash == hash_val and stored_index != dna_num:
-                    if self.config_dict['Virtual_mode'] == False:
-                        self.index_list.append((dna_index, self.ref_dict[stored_index][0]))
-                    else:
-                        self.ref_dict[stored_index].append(dna_tag)
-                    match_found = True
-                    break
-            if match_found:
+        i = 0
+        cluster_num = len(self.front_hash_array)
+        for idx in self.front_hash_array:
+            count = 0
+            front_cluster = self.front_hash_array[idx][0]
+            middle_cluster = self.middle_hash_array[idx][0]
+            back_cluster = self.back_hash_array[idx][0]
+            if front_hash <= front_cluster + self.cluster_hash_threshold and front_hash >= front_cluster - self.cluster_hash_threshold:
+                count += 1
+            if middle_hash <= middle_cluster + self.cluster_hash_threshold and middle_hash >= middle_cluster - self.cluster_hash_threshold:
+                count += 1
+            if back_hash <= back_cluster + self.cluster_hash_threshold and back_hash >= back_cluster - self.cluster_hash_threshold:
+                count += 1
+            
+            if count >= 2: # cluster matches if at least 2 fragments meet cluster threshold
+                match_found = True
+                i = idx
                 break
 
-        if not match_found:
-            self.ref_list[dna_num] = dna_str
+        if match_found:
+            # update fragments cluster count
+            self.front_hash_array[i][1] += 1
+            self.middle_hash_array[i][1] += 1
+            self.back_hash_array[i][1] += 1
+
+            # update fragments cluster hash values count
+            if front_hash in self.front_ref_list[i]:
+                self.front_ref_list[i][front_hash] += 1
+            else:
+                self.front_ref_list[i][front_hash] = 1
+            if middle_hash in self.middle_ref_list[i]:
+                self.middle_ref_list[i][middle_hash] += 1
+            else:
+                self.middle_ref_list[i][middle_hash] = 1
+            if back_hash in self.back_ref_list[i]:
+                self.back_ref_list[i][back_hash] += 1
+            else:
+                self.back_ref_list[i][back_hash] = 1
+
+            # update fragment cluster representative hash value if size / 5 = 0
+            if self.front_hash_array[i][1] == 5:
+                self.update_hash(i, "front")
+            if self.middle_hash_array[i][1] == 5:
+                self.update_hash(i, "middle")
+            if self.back_hash_array[i][1] == 5:
+                self.update_hash(i, "back")
+
+            self.ref_dict[i].append(dna_tag)
+
+        else: # if no matches found, create new cluster for all fragments
+            self.front_hash_array[dna_num] = (front_hash, 1)
+            self.middle_hash_array[dna_num] = (middle_hash, 1)
+            self.back_hash_array[dna_num] = (back_hash, 1)
+            #self.ref_list[dna_num] = dna_str
+            self.front_ref_list[dna_num] = {front_hash: 1}
+            self.middle_ref_list[dna_num] = {middle_hash: 1}
+            self.back_ref_list[dna_num] = {back_hash: 1}
             self.ref_dict[dna_num] = [dna_tag]
     
     def run(self):
